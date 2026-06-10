e18d73a41b879ad972a9508b54a91f835fd2a4aa
spandana-moha <mohapatra.spandana@gmail.com>
2026-06-10 10:09:39 +0530

    Implement document loading with PyPDFLoader

--- 06_Document_Loaders/pdf_loader.py
+++ 06_Document_Loaders/pdf_loader.py
@@ -1,6 +1,6 @@
 from langchain_community.document_loaders import PyPDFLoader
 
-loader = PyPDFLoader("Rent Agreement - Balaji Whitefield - B 402_compressed.pdf")
+loader = PyPDFLoader("sample.pdf")
 docs = loader.load()
 
 print(docs[1])
